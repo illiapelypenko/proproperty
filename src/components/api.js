28e2e373f8c6ee303182e284c3a12badc50ca1c2
Illia <core4690>
2020-09-16 17:14:52 +0300

    Change API kay to valid

--- src/components/api.js
+++ src/components/api.js
@@ -3,7 +3,7 @@ import { state } from '../index';
 const BASE_URL = 'https://realtor.p.rapidapi.com';
 const LOCATIONS_AUTOCOMPLETE_URL = BASE_URL + '/locations/auto-complete';
 const PROPERTIES_FOR_SALE_URL = BASE_URL + '/properties/v2/list-for-sale';
-const API_KEY = '897733c2d3msh13b53444c4f53c4p152d7fjsn04123a994089';
+const API_KEY = '38ab51c595mshb3b30b6eef3338ap1fa61cjsndc842b8881a4';
 
 const propertiesBuffer = new Map();
 
@@ -38,7 +38,9 @@ export async function getProperties() {
   }
 
   try {
-    const URL_PARAMS = encodeURI(`?sort=relevance&city=${city}&limit=500&offset=0&state_code=${state_code}`);
+    const URL_PARAMS = encodeURI(
+      `?sort=relevance&city=${city}&limit=500&offset=0&state_code=${state_code}`
+    );
     const X_RAPID_HOST = 'realtor.p.rapidapi.com';
 
     const res = await fetch(PROPERTIES_FOR_SALE_URL + URL_PARAMS, {
